1ef5d3461b5aea0073dcfc797cdd961110577bf7
LoChingHei <145512379+LoChingHei@users.noreply.github.com>
2024-04-16 16:47:56 -0400

    Update routes.js

--- yacht-application/routes.js
+++ yacht-application/routes.js
@@ -1,27 +1,29 @@
+
 var yacht = require('./controller');
 
 module.exports = (app) => {
-    app.get('/get_yacht/:key', (req, res) => {
-        yacht.query_yacht(req, res);
-    });
 
-    app.get('/add_yacht/:yacht', (req, res) => {
-        yacht.add_yacht(req, res);
+    app.get('/add_Yacht/:yacht', (req, res)=>{
+        yacht.add_Yacht(req, res);
+    })
+    app.get('/get_all_yachts', (req, res) => {
+        yacht.get_all_yachts(req, res);
+        
     });
-
+    
     app.get('/book_yacht/:booking', (req, res) => {
         yacht.book_yacht(req, res);
     });
 
-    app.get('/sign_company/:companysignature', (req, res) => {
+    app.get('/sign_company/:signaturec', (req, res) => {
         yacht.sign_company(req, res);
     });
 
-    app.get('/sign_skipper/:skippersignature', (req, res) => {
+    app.get('/sign_skipper/:signatures', (req, res) => {
         yacht.sign_skipper(req, res);
     });
-
-    app.get('/get_all_yachts', (req, res) => {
-        yacht.get_all_yachts(req, res);
+    
+    app.get('/remove_booking/:key', (req, res) => {
+        yacht.remove_booking(req, res);
     });
-};
+}
