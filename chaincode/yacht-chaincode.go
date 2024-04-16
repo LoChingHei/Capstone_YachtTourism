19b4617815ff6143c0a5e546c1c57f6bd344795e
KirillSpitsyn <zima197211@gmail.com>
2024-04-15 21:21:20 -0400

    Update yacht-chaincode.go

--- chaincode/yacht-chaincode.go
+++ chaincode/yacht-chaincode.go
@@ -26,7 +26,16 @@ type Yacht struct {
 	TimestampFrom     string   `json:"timestampfrom"`
 	TimestampTo       string   `json:"timestampto"`
 	Allbooking        [][]string `json:"allbooking"`
-	IPFS             string   `json:"ipfs"`
+	IPFS              string   `json:"ipfs"`
+	Name              string   `json:"name"`
+    	Model             string   `json:"model"`
+    	Capacity          string   `json:"capacity"`
+    	Description       string   `json:"description"`
+    	Length            string   `json:"length"`
+    	Width             string   `json:"width"`
+    	Amenities         string   `json:"amenities"`
+    	Crew              string   `json:"crew"`
+    	SafetyFeatures    string   `json:"safetyFeatures"`
 }
 
 /*
@@ -82,9 +91,9 @@ func (s *SmartContract) queryYacht(APIstub shim.ChaincodeStubInterface, args []s
  */
 func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) peer.Response {
 	yacht := []Yacht{
-		Yacht{ShipId: "80F 3476", Location:"67.0006, -70.4576", LogBook: "Hash", Owner: "John", SignatureCompany: "", SignatureSkipper:"", Booking:false, TimestampFrom:"", TimestampTo:"", Allbooking: [][]string{{"1713585600", "1713758400"}, {"1714017600", "1714104000"}}, IPFS: "https://bafybeidzrnffghqo35mh6nzdakioweg5zn5ad74jjwgzpsrkishnwrfkky.ipfs.cf-ipfs.com/" },
-		Yacht{ShipId: "35M 3314", Location:"40.6064, -73.6745", LogBook: "Hash", Owner: "Peter", SignatureCompany: "sign", SignatureSkipper:"DID_S", Booking:true, TimestampFrom:"1706954400", TimestampTo:"1707127200", Allbooking: [][]string{}, IPFS: "https://bafybeiexopp6sdls4fix3nwjib4reiytgxoevtb4luur4tlb6glcbm26eu.ipfs.cf-ipfs.com/"},
-		Yacht{ShipId: "92F 8193", Location:"46.147656, -1.163943", LogBook: "Hash", Owner: "Anna", SignatureCompany: "", SignatureSkipper:"DID_S", Booking:false, TimestampFrom:"", TimestampTo:"", Allbooking: [][]string{}, IPFS: "https://bafybeidm5i37kedzbize5cvh6z7n2crexfe4zsfnczfnqcfpb66xixfmfq.ipfs.cf-ipfs.com/" },
+		Yacht{ShipId: "80F 3476", Location:"67.0006, -70.4576", LogBook: "Hash", Owner: "John", SignatureCompany: "", SignatureSkipper:"", Booking:false, TimestampFrom:"", TimestampTo:"", Allbooking: [][]string{{"1713585600", "1713758400"}, {"1714017600", "1714104000"}}, IPFS: "https://bafybeidzrnffghqo35mh6nzdakioweg5zn5ad74jjwgzpsrkishnwrfkky.ipfs.cf-ipfs.com/", Name: "Blue Moon", Model: "Ocean Voyager", Capacity: "10 guests", Description: "Luxury yacht with spacious cabins and stunning ocean views. Perfect for a relaxing getaway.", Length: "50 ft", Width: "20 ft", Amenities: "Jacuzzi, BBQ grill", Crew: "Captain, Chef, Steward", SafetyFeatures: "Life jackets, fire extinguishers", },
+		Yacht{ShipId: "35M 3314", Location:"40.6064, -73.6745", LogBook: "Hash", Owner: "Peter", SignatureCompany: "sign", SignatureSkipper:"DID_S", Booking:true, TimestampFrom:"1706954400", TimestampTo:"1707127200", Allbooking: [][]string{}, IPFS: "https://bafybeiexopp6sdls4fix3nwjib4reiytgxoevtb4luur4tlb6glcbm26eu.ipfs.cf-ipfs.com/", Name: "Green Wave", Model: "Sea Yacht", Capacity: "5 guests", Description: "Luxury yacht with spacious cabins.", Length: "200 ft", Width: "10 ft", Amenities: "Jacuzzi", Crew: "Captain", SafetyFeatures: "Life jackets", },
+		Yacht{ShipId: "92F 8193", Location:"46.147656, -1.163943", LogBook: "Hash", Owner: "Anna", SignatureCompany: "", SignatureSkipper:"DID_S", Booking:false, TimestampFrom:"", TimestampTo:"", Allbooking: [][]string{}, IPFS: "https://bafybeidm5i37kedzbize5cvh6z7n2crexfe4zsfnczfnqcfpb66xixfmfq.ipfs.cf-ipfs.com/", Name: "Flying the Seas", Model: "Sea Voyager", Capacity: "7 guests", Description: "Luxury yacht to travel around the world.", Length: "40 ft", Width: "15 ft", Amenities: "BBQ grill", Crew: "Captain, Chef", SafetyFeatures: "fire extinguishers", },
 	}
 
 	i := 0
@@ -101,10 +110,10 @@ func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) peer.Res
  * Accept five argument - key, ShipId, location, logbook, owner, ipfs
  */
 func (s *SmartContract) addYacht(APIstub shim.ChaincodeStubInterface, args []string) peer.Response {
-	if len(args) != 6 {
+	if len(args) != 14 {
 		return shim.Error("Invalid number of arguments - expecting 6")
 	}
-	var yacht = Yacht{ShipId: args[1], Location: args[2], LogBook: args[3], Owner: args[4], SignatureCompany: "", SignatureSkipper: "", Booking: false, TimestampFrom: "", TimestampTo: "", Allbooking: [][]string{}, IPFS: args[5]}
+	var yacht = Yacht{ShipId: args[1], Location: args[2], LogBook: args[3], Owner: args[4], SignatureCompany: "", SignatureSkipper: "", Booking: false, TimestampFrom: "", TimestampTo: "", Allbooking: [][]string{}, IPFS: args[5], Name: args[6], Model: args[7], Capacity: args[8], Description: args[9], Length: args[10], Width: args[11], Amenities: args[12], Crew: args[13], SafetyFeatures: args[14],}
 	yachtAsBytes, _ := json.Marshal(yacht)
 	err := APIstub.PutState(args[0], yachtAsBytes)
 	if err != nil {
@@ -120,8 +129,8 @@ Book a yacht //done
 accept three argument - key, from date, to date
 */
 func (s *SmartContract) book(APIstub shim.ChaincodeStubInterface, args []string) peer.Response{
-	if len(args) != 3 {
-		return shim.Error("Invalid number of arguments - expecting 3")
+	if len(args) != 12 {
+		return shim.Error("Invalid number of arguments - expecting 12")
 	}
 	yachtAsBytes, _ := APIstub.GetState(args[0])
 	if yachtAsBytes == nil {
@@ -138,6 +147,15 @@ func (s *SmartContract) book(APIstub shim.ChaincodeStubInterface, args []string)
 	yacht.SignatureCompany = ""
 	yacht.TimestampFrom = args[1]
 	yacht.TimestampTo = args[2]
+	yacht.Name = args[3]
+    	yacht.Model = args[4]
+    	yacht.Capacity = args[5]
+    	yacht.Description = args[6]
+    	yacht.Length = args[7]
+    	yacht.Width = args[8]
+    	yacht.Amenities = args[9]
+    	yacht.Crew = args[10]
+    	yacht.SafetyFeatures = args[11]
 	booking := []string{args[1], args[2]}
 	yacht.Allbooking = append(yacht.Allbooking, booking)
 	yachtAsBytes, _ = json.Marshal(yacht)
